1ac05991461922046d60b41e5ed927c720f800b2
hedleyroos <hedleyroos@gmail.com>
2013-01-29 11:23:39 +0200

    Minimal product

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-dfp',
+    version='0.1',
+    description='DFP implementation for Django',
+    long_description = open('README.rst', 'r').read() + open('AUTHORS.rst', 'r').read() + open('CHANGELOG.rst', 'r').read(),
+    author='Praekelt Foundation',
+    author_email='dev@praekelt.com',
+    license='BSD',
+    url='http://github.com/praekelt/django-dfp',
+    packages = find_packages(),
+    install_requires = [
+        'Django',
+    ],
+    include_package_data=True,
+    tests_require=[
+        'django-setuptest>=0.1.2',
+    ],
+    test_suite="setuptest.setuptest.SetupTestSuite",
+    classifiers=[
+        "Programming Language :: Python",
+        "License :: OSI Approved :: BSD License",
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
+    zip_safe=False,
+)
